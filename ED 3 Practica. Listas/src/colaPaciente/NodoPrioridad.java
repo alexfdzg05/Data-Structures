b7aa6168937b26020063ec726eb73a27ad801d61
Alejandro <138217651+alexfdzg05@users.noreply.github.com>
2024-05-31 14:00:08 +0200

    Práctica Listas Uploaded

---
+++ ED 3 Practica. Listas/src/colaPaciente/NodoPrioridad.java
@@ -0,0 +1,45 @@
+/**
+ * Nombre: Alejandro
+ * Apellido: Fernández
+ * Num. Matrícula: bu0024
+ */
+
+package colaPaciente;
+
+import general.Paciente;
+
+public class NodoPrioridad {
+    private int clave;
+    private Cola dato;
+    private NodoPrioridad siguiente;
+
+    public NodoPrioridad(int clave, Cola dato, NodoPrioridad siguiente) {
+        this.clave = clave;
+        this.dato = dato;
+        this.siguiente = siguiente;
+    }
+
+    public NodoPrioridad getSiguiente() {
+        return siguiente;
+    }
+
+    public Cola getDato() {
+        return dato;
+    }
+
+    public void setSiguiente(NodoPrioridad siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public void setCola(Cola dato) {
+        this.dato = dato;
+    }
+
+    public int getClave() {
+        return clave;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+}
---
+++ ED 3 Practica. Listas/src/general/EstadoPaciente.java
@@ -0,0 +1,5 @@
+package general;
+
+public enum EstadoPaciente {
+    alta, hospitalizado;
+}
